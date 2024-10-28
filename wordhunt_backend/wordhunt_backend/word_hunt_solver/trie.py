3aeb33819a916c5a91957716f23f4ca717676eb5
Austin Liu <austin.liu05@yahoo.com>
2024-10-28 12:36:13 -0400

    Added Trie class

--- wordhunt_backend/wordhunt_backend/word_hunt_solver/trie.py
+++ wordhunt_backend/wordhunt_backend/word_hunt_solver/trie.py
@@ -6,3 +6,19 @@ class TrieNode:
 class Trie:
     def __init__(self):
         self.root = TrieNode()
+        
+    def insert(word) -> None:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.isWord = True 
+        
+    def search(word) -> bool:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.isWord
