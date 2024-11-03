54851a0b97a6d0fde73d8149274b0ddedd554329
Austin Liu <austin.liu05@yahoo.com>
2024-11-03 14:45:36 -0500

    Made image more responsive

--- wordhunt_frontend/src/components/startScreen.tsx
+++ wordhunt_frontend/src/components/startScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Container } from 'react-bootstrap';
+import { Button, Container, Row, Col } from 'react-bootstrap';
 import tutorialImage from '../assets/images/tutorial.png';
 
 interface StartScreenProps {
@@ -10,12 +10,20 @@ const StartScreen: React.FC<StartScreenProps> = ({ onStart }) => {
     return (
         <Container className="d-flex flex-column justify-content-center align-items-center vh-100">
             <h1 className="mb-4">WordHunt Online</h1>
-            <p className='text-center text-muted mb-4'>Connect letters together by dragging your mouse/finger. Make as many words as possible.</p>
-            <img
-                src={tutorialImage}
-                alt="Tutorial"
-                className="img-fluid mb-4 w-25"
-            />            
+            <p className="text-center text-muted mb-4">
+                Connect letters together by dragging your mouse/finger. Make as many words as possible.
+            </p>
+
+            <Row className="justify-content-center mb-4">
+                <Col xs={8} sm={6} md={4} lg={3}>
+                    <img
+                        src={tutorialImage}
+                        alt="Tutorial"
+                        className="img-fluid" 
+                    />
+                </Col>
+            </Row>
+
             <Button variant="primary" size="lg" onClick={onStart}>
                 Start Game
             </Button>
