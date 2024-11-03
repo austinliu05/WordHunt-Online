855d2c7092671662a92925f66d7ad322f276ba6e
Austin Liu <austin.liu05@yahoo.com>
2024-11-03 14:42:00 -0500

    Added tutorial to start screen

--- wordhunt_frontend/src/components/startScreen.tsx
+++ wordhunt_frontend/src/components/startScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, Container } from 'react-bootstrap';
+import tutorialImage from '../assets/images/tutorial.png';
 
 interface StartScreenProps {
     onStart: () => void;
@@ -10,6 +11,11 @@ const StartScreen: React.FC<StartScreenProps> = ({ onStart }) => {
         <Container className="d-flex flex-column justify-content-center align-items-center vh-100">
             <h1 className="mb-4">WordHunt Online</h1>
             <p className='text-center text-muted mb-4'>Connect letters together by dragging your mouse/finger. Make as many words as possible.</p>
+            <img
+                src={tutorialImage}
+                alt="Tutorial"
+                className="img-fluid mb-4 w-25"
+            />            
             <Button variant="primary" size="lg" onClick={onStart}>
                 Start Game
             </Button>
