57e83aa8a68c38c18124755a469ccd5e50e703bd
Austin Liu <austin.liu05@yahoo.com>
2024-11-14 17:16:01 -0500

    added .env

--- wordhunt_backend/wordhunt_dictionary_script/config/firebaseConfig.js
+++ wordhunt_backend/wordhunt_dictionary_script/config/firebaseConfig.js
@@ -1,10 +1,14 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('./firebase_admin_private_key.json');
+require('dotenv').config();
 
-// Initialize Firebase Admin SDK
+const admin = require('firebase-admin');
+
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://wordhunt-be669-default-rtdb.firebaseio.com"
+    credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    }),
+    databaseURL: process.env.DATABASE_URL
 });
 
 const db = admin.database();
