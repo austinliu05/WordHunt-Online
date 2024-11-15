e987ff128ae69145ceea3a51a90af90cded415a0
Austin Liu <austin.liu05@yahoo.com>
2024-11-15 14:16:49 -0500

    Bug in constants export

--- wordhunt_backend/cpu/constants.js
+++ wordhunt_backend/cpu/constants.js
@@ -8,19 +8,11 @@ const MEDIUM_LIMITED_DEPTH = 8;
 
 const PREFIXES = ['a', 'be', 'de','dis','ex','in','mis','non','over','pre',
 're','uni','with'];
-const SCORING = {
-    3: 100,
-    4: 400,
-    5: 800,
-    6: 1400,
-    7: 1800,
-    8: 2200,
-};
 
 const VOWELS = 'AEIOU';
 
 const WORD_PRIORITY = 'SCPABTMRDEFGLHINOWVKJQYZXU';
 
-module.exports = { DIRECTIONS, EASY_LIMITED_PATHS, EASY_LIMITED_DEPTH, PREFIXES, VOWELS, WORD_PRIORITY };
+module.exports = { DIRECTIONS, LIMITED_PATHS, EASY_LIMITED_DEPTH, MEDIUM_LIMITED_DEPTH, PREFIXES, VOWELS, WORD_PRIORITY };
 
 
