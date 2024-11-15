cc49493c59fa4a4ef5edd5a924a11ca8a5848947
Austin Liu <austin.liu05@yahoo.com>
2024-11-15 00:23:57 -0500

    Added basic landing page message

--- wordhunt_backend/api/index.js
+++ wordhunt_backend/api/index.js
@@ -1,33 +1,37 @@
-    const express = require('express');
-    const serverless = require('serverless-http');
-    const { processGameBoard } = require('../cpu/main');
-    const allowCors = require('../middleware/cors');
+const express = require('express');
+const serverless = require('serverless-http');
+const { processGameBoard } = require('../cpu/main');
+const allowCors = require('../middleware/cors');
 
-    const app = express();
+const app = express();
 
-    const handleGameBoardRequest = async (req, res) => {
-        const { board, difficulty } = req.body;
-        console.log('Board:', board);
-        console.log('Difficulty:', difficulty);
+const handleGameBoardRequest = async (req, res) => {
+    const { board, difficulty } = req.body;
+    console.log('Board:', board);
+    console.log('Difficulty:', difficulty);
 
-        const result = processGameBoard(board, difficulty);
-        res.json({ paths: result });
-    };
+    const result = processGameBoard(board, difficulty);
+    res.json({ paths: result });
+};
 
-    const testConnection = async (req, res) => {
-        res.json({ message: 'SUCCESS! GET request made.' });
-    };
+const testConnection = async (req, res) => {
+    res.json({ message: 'SUCCESS! GET request made.' });
+};
 
-    app.use(express.json());
-    app.post('/api/data', allowCors(handleGameBoardRequest));
-    app.get('/api/data', allowCors(testConnection));
+app.use(express.json());
 
-    if (process.env.NODE_ENV !== 'production') {
-        const PORT = 3000;
-        app.listen(PORT, () => {
-            console.log(`Server is running locally on http://localhost:${PORT}`);
-        });
-    }
+app.get('/', (req, res) => {
+    res.send('Welcome to the Wordhunt API!');
+});
+app.post('/api/data', allowCors(handleGameBoardRequest));
+app.get('/api/data', allowCors(testConnection));
 
-    module.exports = app; // For local testing
-    module.exports.handler = serverless(app); // For Vercel
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = 3000;
+    app.listen(PORT, () => {
+        console.log(`Server is running locally on http://localhost:${PORT}`);
+    });
+}
+
+module.exports = app; // For local testing
+module.exports.handler = serverless(app); // For Vercel
