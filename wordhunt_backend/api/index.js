8de0640ef589f0946c728d97d2f7731b9d6bf339
Austin Liu <austin.liu05@yahoo.com>
2024-11-15 10:39:28 -0500

    Fixed CORS issue

--- wordhunt_backend/api/index.js
+++ wordhunt_backend/api/index.js
@@ -1,41 +1,16 @@
 const express = require('express');
 const serverless = require('serverless-http');
+const cors = require("cors");
 const { processGameBoard } = require('../cpu/main');
 
 const app = express();
 app.use(express.json());
-
-const allowedOrigins = ['http://localhost:3001', 'https://wordhunt-online.vercel.app'];
-
-const allowCors = (req, res, next) => {
-    const origin = req.headers.origin;
-    
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-        res.setHeader('Access-Control-Allow-Credentials', 'true');
-        res.setHeader(
-            'Access-Control-Allow-Methods',
-            'GET, POST, OPTIONS, PATCH, DELETE, PUT'
-        );
-        res.setHeader(
-            'Access-Control-Allow-Headers',
-            'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-        );
-    }
-
-    if (req.method === 'OPTIONS') {
-        res.status(200).end(); // Preflight response
-        return;
-    }
-    
-    next();
-};
-
-app.use(allowCors); // Apply CORS middleware to all routes
+app.use(cors());
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('Welcome to the Wordhunt API!');
+    res.send(res.getHeaders());
+    res.send('Welcome to the Wordhunt API! v1.0.0');
 });
 
 app.post('/api/data', (req, res) => {
@@ -48,7 +23,7 @@ app.post('/api/data', (req, res) => {
 });
 
 app.get('/api/data', (req, res) => {
-    res.json({ message: 'SUCCESS! GET request made.' });
+    res.send(res.getHeaders());
 });
 
 if (process.env.NODE_ENV !== 'production') {
